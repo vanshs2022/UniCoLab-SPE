f9c607fc34edb3966f950db772c57aa187a73942
Harshit Gaur <harshitgaurofficial2205@gmail.com>
2025-03-24 13:29:04 +0530

    explore-page-comleted

--- FrontPage/app/explore/profile/page.jsx
+++ FrontPage/app/explore/profile/page.jsx
@@ -43,17 +43,13 @@ export default function Profiles() {
   }
 
   return (
-    <div className="xplore bg-[#020825]">
+    <div className="explore bg-[#020825]">
     <Navbar />
-    {/* <h1 className="text-white text-center text-2xl mt-6">Explore Profiles</h1> */}
     <HoverDevCards />
     <div className="profiles grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6 p-6">
       {profiles.map((profile, index) => (
         <Link key={index} href={`/explore/profile/${profile._id}`} passHref>
-          {/* <div className="bg-white/10 backdrop-blur-lg rounded-2xl p-4"> */}
-            {/* <SquishyCard data = {profile} /> */}
             <SquishyCard profile={profile} />
-          {/* </div> */}
         </Link>
       ))}
     </div>
