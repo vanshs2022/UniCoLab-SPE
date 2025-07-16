1a2240b68ce5b02cbcaed080c277d3ae89422f57
Vansh Sharma <vns444555@gmail.com>
2025-07-17 00:14:59 +0530

    deploypath change

--- Backend/Components/firebase.js
+++ Backend/Components/firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./unicolab-405f8-firebase-adminsdk-fbsvc-6b60ddcab3.json");
+const serviceAccount = require("../etc/secrets/unicolab-405f8-firebase-adminsdk-fbsvc-6b60ddcab3.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
