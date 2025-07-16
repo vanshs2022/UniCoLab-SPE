a2af8f90208637b1565ad4d8e1b24f064a192f8a
Vansh Sharma <vns444555@gmail.com>
2025-07-17 02:29:55 +0530

    cors update

--- Backend/src/config/corsURL.js
+++ Backend/src/config/corsURL.js
@@ -1,4 +1,4 @@
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = ["http://localhost:3000", "https://unicolab.onrender.com"];
 
 const corsOptions = {
   origin: function (origin, callback) {
